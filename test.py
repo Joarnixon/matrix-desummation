c3ae7e64cd6bdaaec0b009461204a75f29189ec9
Joarnixon <a.kungurtsev@g.nsu.ru>
2023-10-25 16:45:55 +0300

    renamed

---
+++ test.py
@@ -0,0 +1,10 @@
+from desummation import Desummation
+
+object = Desummation()
+A = [[2, 1], [-2, 5]]
+C = [[2.07, 0.95], [-2.11, 5.14]]
+object.fit(A, 2, n_trials = 2000)
+print(object.predict(A))
+print(object.optimizer.weights)
+print(object.predict(C))
+print(object.optimizer.weights)
